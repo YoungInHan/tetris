bf277755ca404ab41133dcb428d1df80446f6121
Young In (Daniel) Han <youngindanielhan@Hubdocs-MBP-3.office.hubdoc.com>
2018-07-31 14:55:58 -0400

    color shape

--- tetris.js
+++ tetris.js
@@ -1,4 +1,4 @@
-// declare object tetris 
+
 let tetris = {};
 
 tetris.drawPlayField = () => {
@@ -10,8 +10,26 @@ tetris.drawPlayField = () => {
     }
 }
 
+tetris.currentCoor = [
+    {row:1,col:1},
+    {row:1,col:2},
+    {row:2,col:1},
+    {row:2,col:2},
+];
+
+tetris.fillCells = (coordinates, fillColor) => {
+    console.log(fillColor);
+    for (let i = 0; i < coordinates.length; i++) {
+        let row = coordinates[i].row;
+        let col = coordinates[i].col;
+        var $coor = $(`.${row}`).find(`#${col}`);
+        $coor.attr('bgcolor', fillColor);
+    }
+}
+
 $(document).ready(() => {
     tetris.drawPlayField();
+    tetris.fillCells(tetris.currentCoor, 'blue');
 })
 
 
