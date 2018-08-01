28660e5c2d38755eb1d2084705f5688a4ca09873
Young In (Daniel) Han <youngindanielhan@Hubdocs-MBP-3.office.hubdoc.com>
2018-08-01 12:48:21 -0400

    added shapes

--- tetris.js
+++ tetris.js
@@ -9,13 +9,66 @@ tetris.drawPlayField = () => {
         }
     }
 }
+tetris.origin = {row:5,col:5};
+tetris.currentShape = 'T';
+tetris.currentCoor;
 
-tetris.currentCoor = [
-    {row:1,col:1},
-    {row:1,col:2},
-    {row:2,col:1},
-    {row:2,col:2},
-];
+tetris.shapeToCoor = (shape, origin) => {
+    console.log('yea');
+    switch (shape) {
+        case 'L':
+            return [
+                {row: origin.row, col: origin.col},
+                {row: origin.row-1, col: origin.col},
+                {row: origin.row+1, col: origin.col},
+                {row: origin.row+1, col: origin.col+1},
+            ];
+        case 'I':
+            return [
+                {row: origin.row, col: origin.col},
+                {row: origin.row-1, col: origin.col},
+                {row: origin.row+1, col: origin.col},
+                {row: origin.row+2, col: origin.col},
+            ];
+        case 'J':
+            return [
+                {row: origin.row, col: origin.col},
+                {row: origin.row-1, col: origin.col},
+                {row: origin.row+1, col: origin.col},
+                {row: origin.row+1, col: origin.col-1},
+            ];
+        case 'O':
+            return [
+                {row: origin.row, col: origin.col},
+                {row: origin.row-1, col: origin.col},
+                {row: origin.row-1, col: origin.col+1},
+                {row: origin.row, col: origin.col+1},
+            ];
+        case 'Z':
+            return [
+                {row: origin.row, col: origin.col},
+                {row: origin.row, col: origin.col-1},
+                {row: origin.row+1, col: origin.col},
+                {row: origin.row+1, col: origin.col+1},
+            ];
+        case 'T':
+            return [
+                {row: origin.row, col: origin.col},
+                {row: origin.row-1, col: origin.col},
+                {row: origin.row, col: origin.col+1},
+                {row: origin.row, col: origin.col-1},
+            ];
+        case 'S':
+            return [
+                {row: origin.row, col: origin.col},
+                {row: origin.row-1, col: origin.col},
+                {row: origin.row, col: origin.col-1},
+                {row: origin.row-1, col: origin.col+1},
+            ];
+        default: 
+            throw new Error('Unexpected letter passed into shapeToCoor()');
+    }
+}
 
 tetris.fillCells = (coordinates, fillColor) => {
     for (let i = 0; i < coordinates.length; i++) {
@@ -27,23 +80,35 @@ tetris.fillCells = (coordinates, fillColor) => {
 }
 
 tetris.move = function (direction){
+    let onBorder = false;
     this.fillCells(this.currentCoor, '');
     for (let i = 0; i < this.currentCoor.length; i++) {
         if (direction === 'R') {
             this.currentCoor[i].col++;
+            if (this.currentCoor[i].col > 9) {
+                onBorder = true;
+            }
         } else if (direction === 'L') {
             this.currentCoor[i].col--;
+            if (this.currentCoor[i].col < 0) {
+                onBorder = true;
+            }
         }
     }
     this.fillCells(this.currentCoor, 'blue');
+
+    if(onBorder && direction === 'L'){
+        this.move('R');
+    } else if (onBorder && direction === 'R'){
+        this.move('L');
+    }
 }
 
 $(document).ready(() => {
     tetris.drawPlayField();
+    tetris.currentCoor = tetris.shapeToCoor(tetris.currentShape, tetris.origin);
     tetris.fillCells(tetris.currentCoor, 'blue');
-    
     $(document).keydown((e)=> {
-        console.log(e.keyCode);
         if (e.keyCode === 39) {
             tetris.move('R');
         }
